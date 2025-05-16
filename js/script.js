b776d11769336955e456cb9466577a5fcf77223e
Catherine Vu <catherine.vu@stu.ocsb.ca>
2025-05-16 21:20:24 +0000

    js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,41 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Catherine All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Catherine Vu
+// Created on: May 16 2025
 // This file contains the JS functions for index.html
+
+'use strict'
+
+/**
+ * This function calculates division, with remainders
+ */
+// eslint-disable-next-line no-unused-vars
+
+function Calculate() {
+  let counter = 0
+
+  // input
+  let firstNumber = parseFloat(
+    document.getElementById('first-number').value
+  )
+  const secondNumber = parseFloat(
+    document.getElementById('second-number').value
+  )
+
+  // process
+  while (firstNumber >= secondNumber) {
+    firstNumber -= secondNumber
+    counter++
+
+    if (firstNumber < secondNumber) {
+      break
+    }
+  }
+
+  // output
+  document.getElementById('answer').innerHTML =
+    'Your answer is: ' +
+    counter +
+    ' R ' +
+    firstNumber
+}
